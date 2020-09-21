d3a1592171c89d9144832c96785b01fd112577c0
gagip <y02645@gmail.com>
2020-09-21 20:29:36 +0900

    add vote func
    - 투표 기능 추가

--- main.py
+++ main.py
@@ -4,26 +4,31 @@ import os
 import numpy as np
 
 bot = commands.Bot(command_prefix='!')
+
 # 토큰
 path = os.path.dirname(os.path.abspath(__file__))
-t = open(path+"/token.txt", 'r', encoding='utf-8')
+t = open(path + "/token.txt", 'r', encoding='utf-8')
 token = t.read().split()[0]
+
 
 @bot.event
 async def on_ready():
     print('Logged in as')
-    print(bot.user.name) # 봇의 이름 출력
-    print(bot.user.id) # 봇의 고유 ID넘버 출력
+    print(bot.user.name)  # 봇의 이름 출력
+    print(bot.user.id)  # 봇의 고유 ID넘버 출력
     print('------')
+
 
 @bot.command()
 async def join(ctx):
     channel = ctx.author.voice.channel
-    await channel.connect() # PyNaCl 라이브러리 필요
+    await channel.connect()  # PyNaCl 라이브러리 필요
+
 
 @bot.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+
 
 @bot.command()
 async def 도움(ctx):
@@ -34,18 +39,18 @@ async def 도움(ctx):
     embed.add_field(name=f'!투표', value=f'!투표 / 투표장이 투표 세팅을 한 후 투표를 진행합니다.')
     await ctx.send(embed=embed)
 
+
 @bot.command()
 async def 롤자랭(ctx, *dis_member):
     # 현재 음성 채널에 있는 사람들 호출
-    user = ctx.author # 호출한 사람
+    user = ctx.author  # 호출한 사람
     channel = user.voice.channel
-    members = np.array([member.name for member in channel.members])
+    members = [member.name for member in channel.members]
+    print(members)
 
     # 특정 멤버 제외
     if len(dis_member) != 0:
-        print(dis_member)
-        idx_list = [abs(int(member))-1 for member in dis_member]
-        # TODO dis_member의 숫자가 아닐 시 예외처리 해줘야 함
+        idx_list = [abs(int(member)) - 1 for member in dis_member]
         members = np.delete(members, idx_list)
 
     # 샘플링
@@ -59,10 +64,44 @@ async def 롤자랭(ctx, *dis_member):
         embed.add_field(name=members[i], value=f"{random_position[i]}\n")
     await ctx.send(embed=embed)
 
+
 # TODO 투표 기능 추가
 @bot.command()
-async def 투표(ctx):
-    pass
+async def 투표(ctx, title=None, *choice):
+    # 투표 도움말
+    if title is None and choice == ():
+        embed = discord.Embed(title=f'투표 도움말', description=f'개발자: gagip')
+        embed.add_field(name=f'좋아요/싫어요', value=f'!투표 제목')
+        embed.add_field(name=f'복수응답(1-9)', value=f'!투표 제목 내용1 내용2 ...')
+        await ctx.send(embed=embed)
+    # 투표 진행
+    else:
+        embed = discord.Embed(title=title)
+        if choice == ():
+            # 좋아요/싫어요
+            message = await ctx.send(embed=embed)
+            await message.add_reaction('👍')
+            await message.add_reaction('👎')
+        else:
+            # 복수응답(1-10)
+            emoji_list = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣']
+
+            s = ''
+            emoji = iter(emoji_list)
+            for cont in choice:
+                try:
+                    s += f'{next(emoji)} {cont}\n'
+                except ValueError:
+                    await ctx.sent('투표 선택지는 9개까지만 가능합니다.')
+                    return
+
+            embed.add_field(name=s, value='1은 기본적으로 있음, 중복투표 가능')
+            message = await ctx.send(embed=embed)
+
+            for i in range(len(choice)):
+                await message.add_reaction(emoji_list[i])
+
+        
 
 # TODO 롤 전적
 @bot.command()
@@ -74,76 +113,81 @@ async def 롤전적(ctx, id):
     url = f'http://www.op.gg/summoner/userName={id}'
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, features='html.parser')
-    info_data = []
-    def add_data(info):
-        print(info)
+
+    # 사용자 함수
+    def get_data(info):
         if info is not None:
-            if re.search(r"\<.*\>", str(info)) is None: # 태그가 있는지 확인
-                info_data.append(info)
+            if re.search(r"\<.*\>", str(info)) is None:  # 태그가 있는지 확인
+                return info
             else:
-                info_data.append(info.get_text().strip()) # 텍스트 추출
+                return info.get_text().strip()  # 텍스트 추출
         else:
-            info_data.append(None)
-        print(info_data)
+            return 'None'
+
 
     # 해당아이디가 존재하지 않는 경우
     if soup.find('div', class_='SummonerNotFoundLayout'):
         await ctx.send("해당 아이디가 존재하지 않네요. 혹시 오타는 아니시죠?")
         return
 
-    add_data("tier info")
-    info_data.append(id) # id 추가
+    # 소환사 정보
+    user_info = [['소환사 이름', '티어', '리그 포인트', '승리', '패배', '승률']]
+    name = id  # id 추가
     rank_info = soup.find('div', class_='TierRankInfo')
-    add_data(rank_info.find('div', class_='TierRank')) # 솔로랭크 티어 추가
-    if "Unranked" not in info_data: # 언랭이 아니면
-        add_data(rank_info.find('div', class_='TierInfo').find('span', 'LeaguePoints')) # 리그 포인트 추가
-        for i in rank_info.find('div', class_='TierInfo').find('span', 'WinLose').find_all('span'): # 승,패,승률 추가
-            add_data(i)
+    tier = get_data(rank_info.find('div', class_='TierRank'))  # 솔로랭크 티어 추가
+    if tier != 'Unranked':
+        lp = get_data(rank_info.find('div', class_='TierInfo').find('span', 'LeaguePoints'))  # 리그 포인트 추가
+        win = get_data(rank_info.find('span', class_='wins'))  # 승리 수 추가
+        lose = get_data(rank_info.find('span', class_='losses'))  # 패배 수 추가
+        winratio = get_data(rank_info.find('span', class_='winratio'))  # 승률 추가
+    else:
+        lp, win, lose, winratio = 'None', 'None', 'None', 'None'
+    user_info.append([name, tier, lp, win, lose, winratio])
 
     # 모스트 추가
-    add_data("most champ")
+    most_champ_lst = [['챔피언', 'kda', '승률']]
     if soup.find('div', class_='MostChampionContent') is not None:
         most_champ = soup.find('div', class_='MostChampionContent').find_all('div', 'ChampionBox Ranked')
         # 모스트 3 추출
         for champ in most_champ[: min(3, len(most_champ))]:
-            info_data.append(champ.find('div', class_='Face')['title']) # 챔피언 이름
-            add_data(champ.find('span', class_='KDA')) # KDA
-            for i in champ.find('div', class_='Played').find_all('div'): # 승률, 플레이 수
-                add_data(i)
+            champ_name = get_data(champ.find('div', class_='Face')['title'])  # 챔피언 이름 추가
+            kda = get_data(champ.find('span', class_='KDA'))  # KDA
+            champ_winratio = get_data(champ.find('div', class_='WinRatio normal tip'))  # 승률
+            most_champ_lst.append([champ_name, kda, champ_winratio])
 
     # 최근 7일간 랭크 승률
-    add_data("lately 7days")
+    last_7 = [['챔피언 이름', '승률', '승리', '패배']]
     if soup.find('div', class_='Content') is not None:
         for champ in soup.find('div', class_='Content').find_all('div', class_='ChampionWinRatioBox'):
-            info_data.append(champ.find('div', class_='ChampionName')['title'])
-            add_data(champ.find('div', class_='WinRatio'))
-            add_data(champ.find('div', class_='Text Left'))
-            add_data(champ.find('div', class_='Text Right'))
-    add_data("end")
+            champ_name = get_data(champ.find('div', class_='ChampionName')['title'])
+            champ_winratio = get_data(champ.find('div', class_='WinRatio'))
+            champ_win = get_data(champ.find('div', class_='Text Left'))
+            champ_lose = get_data(champ.find('div', class_='Text Right'))
+            last_7.append([champ_name, champ_winratio, champ_win, champ_lose])
 
     # TODO 데이터 전처리
 
     # 데이터 출력
-    def print_data(start, end):
-        start_idx = info_data.index(start)
-        end_idx = info_data.index(end)
-        result = ""
-        if end_idx - start_idx == 1:
-            return "해당 데이터가 존재하지 않습니다."
-        for txt in info_data[start_idx+1:end_idx]:
-            if txt is None:
-                result += "값이 없습니다" + "\n"
-            else:
-                if re.search(r"^[a-zA-Z]+",txt) and start != "tier info":
-                    result += "-------\n"
-                result += txt + "\n"
-        return result
+    def print_data(info_type):
+        result = ''
+        # 해당 데이터가 존재하지 않는다면
+        if len(info_type) == 1:
+            return '해당 정보가 존재하지 않습니다.'
+        else:
+            for row in info_type[1:]:  # 데이터
+                count = 0
+                for data in info_type[0]:  # 해더
+                    result += data + ': ' + row[count] + '\n'
+                    count += 1
+                if row is not info_type[-1]:
+                    result += '-' * 10 + '\n'
+            return result
 
     # TODO 디자인 고안
     embed = discord.Embed(title=f"{id}님 전적", description=f'디버그용')
-    embed.add_field(name=f"Tire info", value=print_data("tier info", "most champ"))
-    embed.add_field(name=f"모스트 챔프", value=print_data("most champ", "lately 7days"))
-    embed.add_field(name=f"최근 전적", value=print_data("lately 7days", "end"))
+    embed.add_field(name=f"Tire info", value=print_data(user_info))
+    embed.add_field(name=f"모스트 챔프", value=print_data(most_champ_lst))
+    embed.add_field(name=f"최근 전적", value=print_data(last_7))
     await ctx.send(embed=embed)
 
 
